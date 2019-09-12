050970a71db7daef1e80113dcec56e850fe23f6e
j190512 <j190512@163.com>
2019-09-12 14:58:40 +0800

    project init

---
+++ vue/vue-cli/pj_two/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router' 
+
+//引入统一默认的css样式
+import './assets/styles/reset.css'
+//引入解决移动端1像素问题的css文件
+import './assets/styles/border.css'
+//引入fastClick
+import fastClick from 'fastclick'
+fastClick.attach(document.body)
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
